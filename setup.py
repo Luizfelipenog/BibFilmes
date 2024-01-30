c946b9cdf45965fbe3a626cacfad11d4631f5f6e
Luizfelipenog <Luiz.nogueira@ufpi.edu.br>
2024-01-30 10:04:18 -0300

    organização

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as arq:
 
 setup(
     name='BibFilmes',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT License',
     author='Luiz Felipe',
     long_description=readme,
